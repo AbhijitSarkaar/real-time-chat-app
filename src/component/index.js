2c581ff1ff41f67b5b57f7b82babca6db2e46b45
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2019-06-16 17:05:56 +0530

    set up the structure

---
+++ src/component/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './Main';
+import './index.scss';
+
+function App() {
+  return (
+    <div className="chat-app">
+        <Main />
+    </div>
+  );
+}
+
+export default App;
