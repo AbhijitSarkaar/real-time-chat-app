039bcc8c9520b6acae95fdebbfc1a3ac22809b2f
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2019-07-01 01:07:59 +0530

    created the Log in screen

--- src/component/index.js
+++ src/component/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Main from './Main';
 import './index.scss';
 
-function App() {
-  return (
-    <div className="chat-app">
-        <Main />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userNameSubmitted: false,
+      username: ''
+    }
+  }
+  render() {
+    if(this.state.userNameSubmitted) {
+      return (
+        <div className="chat-app">
+            <Main />
+        </div>
+      );
+    }
+    return <div className="chat-app">
+              <div className="username-input">
+                  <div className="username-header">
+                    <p> Chat application  </p>
+                  </div>
+                  <div className="user-input">
+                    <input placeholder={'Enter your username...'} value={this.state.username} onChange={(e) => this.setState({username: e.target.value})} />
+                    <div className="submit" onClick={() => this.state.username.length && this.setState({userNameSubmitted: true})}>
+                      <p>Submit</p>
+                    </div>
+                  </div>
+              </div>
+          </div>
+  }
 }
 
 export default App;
