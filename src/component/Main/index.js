cdfb45d2fecaf0286b013f1dff6836fa70f6efca
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2019-07-04 02:35:03 +0530

    comments

--- src/component/Main/index.js
+++ src/component/Main/index.js
@@ -9,11 +9,12 @@ export default class Main extends Component {
     constructor(props) {
         super(props);
         this.socket = io('http://localhost:5000', { query: `username=${this.props.username}` }).connect();
-        //sending the username to the server when the connection is established
+        //sending the username to the server when the connection is established. The server is established at 5000 port.
+        //It's present in the chat-api folder
         this.state = {
             messages: []
         }
-        //
+        //getting notified from the server
         this.socket.on('server', message => {
             this.addMessage(message);
         });
@@ -30,7 +31,7 @@ export default class Main extends Component {
             username: this.props.username,
             message: text
         }
-        this.socket.emit('client', message);
+        this.socket.emit('client', message);//notifying the server that a new message has been added.
         this.addMessage(message);
     }
     render() {
